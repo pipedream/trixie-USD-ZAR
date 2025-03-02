1d508cf979efe90a261213295ecfcc43feabedde
Makevich <149294123+Makev1ch@users.noreply.github.com>
2025-03-02 11:02:09 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,14 +1,14 @@
-// This extensions shows USD to TRY convertion on Gnome panel.
-//Copyright (C) 2023  arfiesat
+// This extension shows USD to RUB conversion on Gnome panel.
+// Copyright (C) 2023  arfiesat (modified for USD-RUB)
 // See LICENSE file
 
 'use strict';
 
-import St from 'gi://St'
-import Gio from 'gi://Gio'
-import Clutter from 'gi://Clutter'
-import Soup from 'gi://Soup'
-import GLib from 'gi://GLib'
+import St from 'gi://St';
+import Gio from 'gi://Gio';
+import Clutter from 'gi://Clutter';
+import Soup from 'gi://Soup';
+import GLib from 'gi://GLib';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
@@ -18,7 +18,7 @@ let session;
 let dollarQuotation;
 let sourceId = null;
 
-// Handle Requests API Dollar
+// Handle Requests API for USD-RUB
 async function handle_request_dollar_api() {
     try {
         // Create a new Soup Session
@@ -26,9 +26,11 @@ async function handle_request_dollar_api() {
             session = new Soup.Session({ timeout: 10 });
         }
 
-        // Create body of Soup request
-        let message = Soup.Message.new_from_encoded_form(
-            "GET", "https://economia.awesomeapi.com.br/last/USD-TRY", Soup.form_encode_hash({}));
+        // API URL for USD-RUB (Центральный Банк России)
+        const apiUrl = 'https://www.cbr-xml-daily.ru/daily_json.js';
+
+        // Create Soup request
+        let message = Soup.Message.new('GET', apiUrl);
 
         // Send Soup request to API Server
         await session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (_, r0) => {
@@ -36,15 +38,14 @@ async function handle_request_dollar_api() {
             let response = new TextDecoder().decode(text.get_data());
             const body_response = JSON.parse(response);
 
-            // Get the value of Dollar Quotation
-            dollarQuotation = body_response["USDTRY"]["bid"];
-            dollarQuotation = dollarQuotation.split(".");
-            dollarQuotation = dollarQuotation[0] + "," + dollarQuotation[1].substring(0, 2);
+            // Get the value of USD-RUB
+            dollarQuotation = body_response["Valute"]["USD"]["Value"];
+            dollarQuotation = dollarQuotation.toFixed(2); // Округляем до двух знаков после запятой
 
-            // Sext text in Widget
+            // Set text in Widget
             panelButtonText = new St.Label({
-            style_class : "cPanelText",
-                text: "(1 USD = " + dollarQuotation + " TRY)",
+                style_class: "cPanelText",
+                text: `(1 USD = ${dollarQuotation} RUB)`,
                 y_align: Clutter.ActorAlign.CENTER,
             });
             panelButton.set_child(panelButtonText);
@@ -57,7 +58,7 @@ async function handle_request_dollar_api() {
     } catch (error) {
         console.error(`Traceback Error in [handle_request_dollar_api]: ${error}`);
         panelButtonText = new St.Label({
-            text: "(1 USD = " + _dollarQuotation + ")" + " * ",
+            text: "USD = N/A RUB",
             y_align: Clutter.ActorAlign.CENTER,
         });
         panelButton.set_child(panelButtonText);
@@ -70,30 +71,37 @@ export default class Extension {
         panelButton = new St.Bin({
             style_class: "panel-button",
         });
-    
+
+        // Initial API call
         handle_request_dollar_api();
+
+        // Add the button to the panel
         Main.panel._centerBox.insert_child_at_index(panelButton, 0);
-        sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
+
+        // Update the rate every 5 minutes (300 seconds)
+        sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 300, () => {
             handle_request_dollar_api();
             return GLib.SOURCE_CONTINUE;
         });
     }
 
     disable() {
+        // Remove the button from the panel
         Main.panel._centerBox.remove_child(panelButton);
 
+        // Clean up resources
         if (panelButton) {
             panelButton.destroy();
             panelButton = null;
         }
-    
+
         if (sourceId) {
             GLib.Source.remove(sourceId);
             sourceId = null;
         }
-        
+
         if (session) {
-            session.abort(session);
+            session.abort();
             session = null;
         }
     }
